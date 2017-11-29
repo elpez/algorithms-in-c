7741da949463effd75c78ff77a099a76f5cb05cc
Ian Fisher <ianfisher45@gmail.com>
2017-11-29 17:03:39 -0500

    Add introduction and section headers in algorithms.c

--- algorithms.c
+++ algorithms.c
@@ -1,4 +1,30 @@
+/*  Welcome! This file contains C implementations of many of the algorithms presented in Anany
+ *  Levitin's textbook "Introduction to the Design and Analysis of Algorithms." The file, like the
+ *  textbook, organizes algorithms into seven design classes: brute-force, decrease-and-conquer,
+ *  divide-and-conquer, dynamic programming, the greedy technique, and iterative improvement. The
+ *  file is intended to be read sequentially, but I have tried to document my code comprehensively
+ *  enough that you may read the implementation of any particular algorithm in isolation.
+ *
+ *  Each algorithm is preceded by a comment that (a) documents its usage as a C function, (b)
+ *  sketches the idea of the algorithm in prose, and (c) analyzes the time and space complexity.
+ *  Details of the algorithm's implementation are documented as inline comments in the code.
+ *
+ *  Basic knowledge of C is required to understand these algorithms. I have avoided hacks and
+ *  optimizations whenever they would reduce the clarity of the code, and I have made some
+ *  simplifications of implementation, for example only writing sorting algorithms for arrays of
+ *  integers. For these two reasons, the functions here should not be taken as substitutes for
+ *  canonical implementations.
+ *
+ *  The bulk of this file consists of the algorithms themselves. At the bottom are implementations
+ *  of the utility functions defined in algorithms.h, and a test suite and main function.
+ */
+
 #include "algorithms.h"
+
+
+/*****************************************************
+ *   CHAPTER 3 - BRUTE FORCE and EXHAUSTIVE SEARCH   *
+ *****************************************************/
 
 
 /* Sort the elements of `array` in place.
@@ -8,8 +34,8 @@
  *
  *   Time analysis: The outer loop runs n times, so n swaps are made. When i=0, the inner loop
  *   runs n-1 times, when i=1 n-2 times, and so on for a total of n-1 + n-2 + ... + 1 times,
- *   which is equal to n(n-1)/2. This is asymptotically equivalent to n^2, so O(n^2) comparisons
- *   are made. Therefore the overall time complexity is O(n^2).
+ *   which equals n(n-1)/2. This is asymptotically equivalent to n^2, so O(n^2) comparisons are
+ *   made. Therefore the overall time complexity is O(n^2).
  *
  *   Space analysis: No extra space is used, so O(1).
  */
@@ -32,11 +58,21 @@ void selection_sort(int array[], size_t n) {
 }
 
 
+/*************************
+ *   UTILITY FUNCTIONS   *
+ *************************/
+
+
 void swap(int array[], size_t i, size_t j) {
     int tmp = array[i];
     array[i] = array[j];
     array[j] = tmp;
 }
+
+
+/***************************
+ *   TEST SUITE and MAIN   *
+ ***************************/
 
 
 void run_tests(void) {
